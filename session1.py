df3e1dad35812271782237b89e379e98b20d63a0
Roh hedayati <roh.hedayati@gmail.com>
2025-08-02 20:05:11 +0330

    First session file

---
+++ session1.py
@@ -0,0 +1,19 @@
+# How to use print function
+
+print('hello world')        
+print('Hedayati')       
+print(12)
+last_name = 'Hedayati'
+print(last_name)
+
+int_var = 10
+float_var = 3.14
+str_var = 'Hello World'
+is_single = False
+
+print(False)
+print(10)
+# print(10.5)
+# print('Hello World')
+# print(True)
+# print(False) 
