ccc63fc91b4215d3bfa514418020b61cb16c0bf6
Roh hedayati <roh.hedayati@gmail.com>
2025-08-02 20:16:59 +0330

    فایل های جلسه سوم

---
+++ session3.py
@@ -0,0 +1,43 @@
+x = 10
+x = x * 5
+x *= 5
+
+
+# عملگرهای مقایسه ای
+#>  > =  <   < =   ==   ! =
+y = 15
+z = 4
+#print(y == z)
+
+# عملگرهای منطقی
+# and  or  not
+True_value = True
+False_value = False
+#print(True_value or False_value)
+
+# IF
+#age = int(input("Enter your age:"))
+
+#if age >= 10 :
+#    print("Welcome")
+#else:
+#    print("Not Allowed")
+
+#print("End") 
+'''
+number = int(input("Enter a number:"))
+if number % 2:
+    print("Odd number")
+else:
+    print("Even number")
+'''
+
+num = int(input("Enter a number:"))
+if num > 0 :
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero Number")
+
+10 >= x >= 1  # x <= 10 and x>=1
