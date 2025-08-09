14ac8d8c43afa1ef87596a39774845a3cf802fdd
Roh hedayati <roh.hedayati@gmail.com>
2025-08-09 20:22:19 +0330

    فایل جلسه نهم

---
+++ list.py
@@ -0,0 +1,41 @@
+number_list = [10,15,20,17,36]
+names_list = ['ali', 'mahdi', 'mina']
+boolean_list = [True, False, False]
+all_list = ['ali', 15, True]
+empty_list = []
+# print(number_list[2])
+# print(type(boolean_list))
+# x = list((1,2,3,4,5))
+
+# number_list[4] = 50
+# print(number_list)
+
+# number_list.append(100)
+# names_list.append('sara')
+# all_list.append(17.25)
+# empty_list.append(15)
+
+# print(number_list)
+# print(names_list)
+# print(all_list)
+# print(empty_list)
+
+# number_list.insert()
+# print(number_list)
+
+
+
+['ali', 'mina', 'sara']
+[15.25, 10, 17]
+
+# name   grade
+# ali    15.25
+# mina   10
+# sara   17
+
+# print(len(number_list))
+# for i in range(len(number_list)):
+#     print(number_list[i])
+
+for x in number_list:
+    print(x)
